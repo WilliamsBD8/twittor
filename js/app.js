bfa2f433a2f2f1b0bfc7c8c96275a8d75a3f3242
WilliamBD8 <wsbonilladiaz@gmail.com>
2021-07-20 21:16:51 -0500

    Cambio url 2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ var url = window.location.href;
 var swLocation = 'twittor/sw.js';
 
 if(navigator.serviceWorker){
-    if(url.includes('localhost')) swLocation = '/sw.js';
+    if(url.includes('localhost'))
+        swLocation = '/sw.js';
     navigator.serviceWorker.register('/sw.js');
 }
 // Referencias de jQuery
